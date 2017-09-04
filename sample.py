c21efb57b2ac016322aac4db6bcdf49581a27e66
billjyc <793746995@qq.com>
2017-09-04 11:42:38 +0800

    sample.py增加调试

--- sample.py
+++ sample.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from qqbot.utf8logger import DEBUG
+import json
 
 def onInit(bot):
     # 初始化时被调用
@@ -24,7 +25,10 @@ def onQQMessage(bot, contact, member, content):
     # contact : QContact 对象，消息的发送者
     # member  : QContact 对象，仅当本消息为 群或讨论组 消息时有效，代表实际发消息的成员
     # content : str 对象，消息内容
-    if content == '--version' and getattr(member, 'uin') == bot.conf.qq:
+    DEBUG('member: %s', str(getattr(member, 'uin')))
+    DEBUG('content: %s', content)
+    DEBUG('contact: %s', json.dumps(contact))
+    if content == '--version':
         bot.SendTo(contact, 'QQbot-' + bot.conf.version)
 
 def onInterval(bot):
