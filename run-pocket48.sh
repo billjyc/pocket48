689be60396defb8ccf8c5e8bb82945d5987d1d68
billjyc <793746995@qq.com>
2017-09-06 10:06:25 +0800

    修改启动脚本

--- run-pocket48.sh
+++ run-pocket48.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 echo stop application
-PID=$(ps -ef | grep 'pocket48_handler' | grep -v grep | awk '{print $2}')
-if [-z PID]
-then
-    echo Application is already stopped
-else
-    echo kill $PID
-    kill $PID
-fi
+qq stop
 
-echo start application
-cd /home/pocket48
 git pull
 
-nohup python pocket48_handler.py > pocket48.log 2>&1 &
+echo start qqbot
+# 将用户名换成在conf中填写的用户名
+qqbot -u 'fxftest'
