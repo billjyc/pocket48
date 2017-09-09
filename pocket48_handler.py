d0021433c3e2bf3e1260fe8bcdd8df44bc1242c0
billjyc <billjyc@gmail.com>
2017-09-09 15:21:32 +0800

    修正了重复发送的bug

--- pocket48_handler.py
+++ pocket48_handler.py
@@ -31,7 +31,7 @@ class Pocket48Handler:
         """
         url = 'https://pjuju.48.cn/imsystem/api/im/v1/member/room/message/chat'
         params = {
-            "roomId": room_id, "lastTime": 0, "limit": 5
+            "roomId": room_id, "lastTime": 0, "limit": 10
         }
         try:
             response = requests.post(url, data=json.dumps(params), headers=self.header_args(), verify=False)
@@ -68,7 +68,7 @@ class Pocket48Handler:
 
     def parse_room_msg(self, response):
         """
-        对响应进行处理
+        对成员消息进行处理
         :param response:
         :return:
         """
@@ -79,7 +79,7 @@ class Pocket48Handler:
         message = ''
         for msg in msgs:
             extInfo = json.loads(msg['extInfo'])
-            platform = extInfo['platform']
+            # platform = extInfo['platform']
             msg_id = msg['msgidClient']
 
             # bodys = json.loads(msg['bodys'])
@@ -87,51 +87,60 @@ class Pocket48Handler:
             # if self.last_monitor_time < 0 or msg['msgTime'] < temp_timestamp:
             #     break
             # 判断是否为成员
-            if self.is_member(extInfo['senderRole']):
-                if msg_id in self.member_room_msg_ids:
-                    continue
-                DEBUG('成员消息')
-                self.member_room_msg_ids.append(msg_id)
-                DEBUG('extInfo.keys():' + ','.join(extInfo.keys()))
-                if 'text' in extInfo.keys():  # 普通消息
-                    DEBUG('普通消息')
-                    message += '【成员消息】[%s]-%s: %s\n' % (msg['msgTimeStr'], extInfo['senderName'], extInfo['text'])
-                elif 'messageText' in extInfo.keys():  # 翻牌消息
-                    DEBUG('翻牌')
-                    member_msg = extInfo['messageText']
-                    fanpai_msg = extInfo['faipaiContent']
-                    fanpai_id = extInfo['faipaiName']
-                    message += '【翻牌】[%s]-%s\n【被翻牌】冯晓菲的%s:%s\n' % (msg['msgTimeStr'], member_msg, fanpai_id, fanpai_msg)
-                # elif self.check_json_format(msg['bodys']):  # 图片
-                #     DEBUG('图片消息')
-                #     bodys = json.loads(msg['bodys'])
-                #     if 'url' in bodys.keys():
-                #         url = bodys['url']
-                #         message += '【图片】[%s]-%s\n' % (msg['msgTimeStr'], url)
-                else:
-                    is_json = self.check_json_format(msg['bodys'])
-                    bodys = json.loads(msg['bodys'])
-                    if 'url' in bodys.keys():
-                        url = bodys['url']
-                        DEBUG('图片')
-                        message += '【图片】[%s]-%s\n' % (msg['msgTimeStr'], url)
-                    else:
-                        DEBUG('房间语音')
-                if message and len(self.member_room_msg_groups) > 0:
-                    QQHandler.send_to_groups(self.member_room_msg_groups, message)
+            if msg_id in self.member_room_msg_ids:
+                continue
+            DEBUG('成员消息')
+            self.member_room_msg_ids.append(msg_id)
+            DEBUG('extInfo.keys():' + ','.join(extInfo.keys()))
+            if 'text' in extInfo.keys():  # 普通消息
+                DEBUG('普通消息')
+                message += '【成员消息】[%s]-%s: %s\n' % (msg['msgTimeStr'], extInfo['senderName'], extInfo['text'])
+            elif 'messageText' in extInfo.keys():  # 翻牌消息
+                DEBUG('翻牌')
+                member_msg = extInfo['messageText']
+                fanpai_msg = extInfo['faipaiContent']
+                fanpai_id = extInfo['faipaiName']
+                message += '【翻牌】[%s]-%s\n【被翻牌】冯晓菲的%s:%s\n' % (msg['msgTimeStr'], member_msg, fanpai_id, fanpai_msg)
             else:
-                if msg_id in self.member_room_comment_ids:
-                    continue
-                DEBUG('房间评论')
-                self.member_room_comment_ids.append(msg_id)
-                message += '【房间评论】[%s]-%s: %s\n' % (msg['msgTimeStr'], extInfo['senderName'], extInfo['text'])
-                if message and len(self.member_room_comment_msg_groups) > 0:
-                    QQHandler.send_to_groups(self.member_room_comment_msg_groups, message)
+                is_json = self.check_json_format(msg['bodys'])
+                bodys = json.loads(msg['bodys'])
+                if 'url' in bodys.keys():
+                    url = bodys['url']
+                    DEBUG('图片')
+                    message += '【图片】[%s]-%s\n' % (msg['msgTimeStr'], url)
+                else:
+                    DEBUG('房间语音')
+
+        if message and len(self.member_room_msg_groups) > 0:
+            QQHandler.send_to_groups(self.member_room_msg_groups, message)
         INFO('message: %s', message)
         DEBUG('成员消息队列: %s', len(self.member_room_msg_ids))
-        DEBUG('房间评论队列: %s', len(self.member_room_comment_ids))
 
-        # print '[%s]-%s: %s' % (msg['msgTimeStr'], extInfo['senderName'], extInfo['text'])
+    def parse_room_comment(self, response):
+        """
+        对房间评论进行处理
+        :param response:
+        :return:
+        """
+        rsp_json = json.loads(response)
+        msgs = rsp_json['content']['data']
+
+        message = ''
+        for msg in msgs:
+            extInfo = json.loads(msg['extInfo'])
+            platform = extInfo['platform']
+            msg_id = msg['msgidClient']
+
+            if msg_id in self.member_room_comment_ids:
+                continue
+            DEBUG('房间评论')
+            self.member_room_comment_ids.append(msg_id)
+            message += '【房间评论】[%s]-%s: %s\n' % (msg['msgTimeStr'], extInfo['senderName'], extInfo['text'])
+
+        INFO('message: %s', message)
+        if message and len(self.member_room_comment_msg_groups) > 0:
+            QQHandler.send_to_groups(self.member_room_comment_msg_groups, message)
+        DEBUG('房间评论队列: %s', len(self.member_room_comment_ids))
 
     def get_member_room_comment(self, room_id):
         """
