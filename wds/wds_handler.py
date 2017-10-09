c16599de716cef2c8e0455710129d10abf784b23
billjyc <793746995@qq.com>
2017-10-09 16:24:13 +0800

    集资进度情况通过爬虫（beautifulsoup4）来获取

--- wds/wds_handler.py
+++ wds/wds_handler.py
@@ -6,6 +6,7 @@ import json
 import time
 from qqhandler import QQHandler
 from qqbot.utf8logger import INFO, ERROR, DEBUG
+from bs4 import BeautifulSoup
 
 import Queue
 import utils
@@ -52,8 +53,47 @@ class WDSHandler():
             'pro_id': pro_id
         }
         try:
-            r = self.session.post(jizi_url, data=json.dumps(params), header=self.wds_header(), verify=False)
+            r = self.session.post(jizi_url, data=json.dumps(params), headers=self.wds_header(), verify=False)
         except Exception as e:
             ERROR('获取微打赏评论失败')
             ERROR(e)
         return r.json()
+
+    def get_current_and_target(self):
+        """
+        获取当前进度和总额
+        :return:
+        """
+        header = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Encoding': 'gzip,deflate,sdch,br',
+            'Accept-Language': 'zh-CN,zh;q=0.8',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
+            'Host': 'wds.modian.com',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.104 Safari/537.36 Core/1.53.3408.400 QQBrowser/9.6.12028.400'
+        }
+        r = requests.get('https://wds.modian.com/show_weidashang_pro/7974', headers=header)
+        soup = BeautifulSoup(r.text, 'lxml')
+        # print soup.prettify()
+
+        project_info = soup.find_all(class_="project-info")[0]
+        support_num = project_info.find_all(class_="b")[0].find_all(name="span")[0].contents[0].strip()
+        DEBUG('当前集资人数: %s', support_num)
+        print support_num
+
+        current = project_info.find_all(class_="current")[0].find_all(name="span")[1].contents[1].strip()
+        DEBUG('当前进度: %s', current)
+        print current
+        target = project_info.find_all(class_="target")[0].find_all(name="span")[1].contents[1].strip()
+        DEBUG('目标金额: %s', target)
+        print target
+
+        msg = '当前进度: %s, 目标金额: %s; 当前集资人数: %s' % (current, target, support_num)
+        return msg
+
+
+if __name__ == '__main__':
+    handler = WDSHandler()
+    handler.get_current_and_target()
