962dbfd0cc58a31162d48c7e9e62aa171aaf9c1b
billjyc <793746995@qq.com>
2017-10-09 19:38:16 +0800

    bug fixed

--- wds_plugin.py
+++ wds_plugin.py
@@ -9,19 +9,21 @@ from qqhandler import QQHandler
 
 
 wds_handler = None
+qq_handler = None
 
 
 def onStartupComplete(bot):
     # 启动完成时被调用
     # bot : QQBot 对象，提供 List/SendTo/GroupXXX/Stop/Restart 等接口，详见文档第五节
-    global wds_handler
+    global wds_handler, qq_handler
     wds_handler = WDSHandler([])
+    qq_handler = QQHandler()
     update_wds_conf(bot)
 
 
 @qqbotsched(minute='*', second="45")
 def update_wds_conf(bot):
-    global wds_handler
+    global wds_handler, qq_handler
 
     DEBUG('读取微打赏配置')
     ConfigReader.read_conf()
@@ -41,7 +43,8 @@ def update_wds_conf(bot):
     DEBUG('wds pro id: %s', global_config.WDS_PRO_ID)
 
     global_config.JIZI_NOTIFY_GROUPS = ConfigReader.get_property('qq_conf', 'jizi_notify_groups').split(';')
-    wds_handler.wds_notify_groups = global_config.JIZI_NOTIFY_GROUPS
+    wds_groups = qq_handler.list_group(global_config.JIZI_NOTIFY_GROUPS)
+    wds_handler.wds_notify_groups = wds_groups
 
     DEBUG('JIZI_NOTIFY_GROUPS: %s, length: %d', ','.join(global_config.JIZI_NOTIFY_GROUPS),
           len(wds_handler.wds_notify_groups))
