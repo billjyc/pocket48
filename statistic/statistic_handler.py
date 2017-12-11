75b5289a0fc51b2e355ae76f76df01b7650369b2
billjyc <793746995@qq.com>
2017-12-11 22:42:31 +0800

    增加统计模块，可以统计每天的应援群人数

---
+++ statistic/statistic_handler.py
@@ -0,0 +1,96 @@
+# -*- coding:utf-8 -*-
+
+import sys
+
+import time
+import sqlite3
+import requests
+
+from qq.qqhandler import QQHandler
+from qqbot.utf8logger import DEBUG, INFO, ERROR
+from utils import util
+
+from bs4 import BeautifulSoup
+
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+class StatisticHandler:
+    def __init__(self, db_path):
+        self.session = requests.session()
+        self.conn = sqlite3.connect(db_path)
+        DEBUG('读取数据库成功')
+
+    def update_group_size(self, member_name):
+        """
+        获取群人数
+        :param member_name:
+        :return:
+        """
+        cursor = self.conn.cursor()
+        QQHandler.update()
+
+        try:
+            # 获取群号
+            c = cursor.execute("""
+                select group_number from member WHERE member_name=?
+            """, (member_name, ))
+            group_number = c.fetchone()[0]
+
+            number = QQHandler.get_group_number(group_number)
+
+            # number = 800
+            cur_date = util.convert_timestamp_to_timestr(time.time() * 1000)
+
+            DEBUG('统计：成员: %s, 群号: %s, 人数: %d, 时间: %s', member_name, group_number, number, cur_date)
+            cursor.execute("""
+            INSERT INTO `group` (`member_name`, `group_number`, `group_size`, `date`) VALUES
+            (?, ?, ?, ?)
+            """, (member_name, group_number, number, cur_date))
+            self.conn.commit()
+        except Exception as e:
+            ERROR(e)
+        finally:
+            cursor.close()
+
+    def get_super_tag_size(self, member_name):
+        """
+        获取超话人数
+        :param member_name:
+        :return:
+        """
+        cursor = self.conn.cursor()
+
+        try:
+            # 获取群号
+            c = cursor.execute("""
+                        select super_tag from member WHERE member_name=?
+                    """, (member_name,))
+            super_tag = c.fetchone()[0]
+
+            r = self.session.get(super_tag)
+            soup = BeautifulSoup(r.content, 'lxml')
+
+            tb_counter = soup.find_all(class_='tb_counter')[0]
+            fans_number = tb_counter.find_all(class_='S_line1')[2].find_all('strong').contents[0]
+
+            cur_date = util.convert_timestamp_to_timestr(time.time() * 1000)
+
+            DEBUG('统计：成员: %s, 超话: %s, 人数: %d, 时间: %s', member_name, super_tag, number, cur_date)
+            cursor.execute("""
+                    INSERT INTO `super_tag` (`member_name`, `link`, `size`, `date`) VALUES
+                    (?, ?, ?, ?)
+                    """, (member_name, super_tag, number, cur_date))
+            self.conn.commit()
+        except Exception as e:
+            ERROR(e)
+        finally:
+            cursor.close()
+
+
+if __name__ == "__main__":
+    statistic_handler = StatisticHandler('statistics.db')
+    # statistic_handler.update_group_size('fengxiaofei')
+    statistic_handler.get_super_tag_size('fengxiaofei')
---
+++ statistic_plugin.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from qqbot import qqbotsched
+from qqbot.utf8logger import DEBUG
+from utils.config_reader import ConfigReader
+from statistic.statistic_handler import StatisticHandler
+from utils import global_config
+from qq.qqhandler import QQHandler
+import json
+
+
+statistic_handler = None
+
+
+def onStartupComplete(bot):
+    # 启动完成时被调用
+    # bot : QQBot 对象，提供 List/SendTo/GroupXXX/Stop/Restart 等接口，详见文档第五节
+    global statistic_handler
+    statistic_handler = statistic_handler()
+    update_wds_conf(bot)
+
+
+@qqbotsched(hour='2', minute="45")
+def update_wds_conf(bot):
+    global statistic_handler
+
+    DEBUG('读取数据配置')
+    ConfigReader.read_conf()
+
+
+@qqbotsched(hour='3')
+def record_data(bot):
+    """
+    记录数据
+    :param bot:
+    :return:
+    """
+    global statistic_handler
+    DEBUG('记录数据')
+    statistic_handler.update_group_size(global_config.MEMBER_NAME)
+
+
+if __name__ == '__main__':
+    pass
