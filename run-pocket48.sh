47841e3427d72d7d16e6aecf1402520661a4f175
billjyc <billjyc@gmail.com>
2018-01-07 23:32:15 +0800

    bug fixed

--- run-pocket48.sh
+++ run-pocket48.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "start coolq server"
-nohup python3 cool_http_server.py > /dev/null 2&>1
+nohup python3 cool_http_server.py > /dev/null 2&>1 &
 
 echo "start service"
-nohup python3 main.py > /dev/null 2&>1
+nohup python3 main.py > /dev/null 2&>1 &
 
