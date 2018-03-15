a8fd45bc1199da8bdc48227eb5c3e557ca1674e5
billjyc <793746995@qq.com>
2018-03-15 08:57:36 +0800

    如果订单队列为空，则不对订单页进行处理（防止误报）

--- modian/modian_handler.py
+++ modian/modian_handler.py
@@ -124,6 +124,7 @@ class ModianHandler:
 
     def parse_order_details(self, orders, modian_entity):
         if len(self.order_queues[modian_entity.pro_id]) == 0:
+            my_logger.debug('订单队列为空')
             return
         # time_tmp = time.time()
         jiebang_activities = global_config.MODIAN_JIEBANG_ACTIVITIES[modian_entity.pro_id]
