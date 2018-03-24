5996566825bda3fa968b7ae484aba059a13d6f13
billjyc <793746995@qq.com>
2018-03-24 11:04:28 +0800

    封装mysql操作

---
+++ utils/mysql_util.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+import pymysql
+from log.my_logger import logger
+
+
+class MySQLUtil:
+    def __init__(self):
+        self.conn = self.getConn()
+
+    def getConn(self, host, port, user, passwd, db, charset="utf8"):
+        logger.debug('获取数据库连接')
+        try:
+            conn = pymysql.connect(host=host, port=port, passwd=passwd, db=db, user=user, charset=charset)
+            return conn
+        except pymysql.Error as e:
+            logger.error('连接mysql出现错误: %s', e)
+
+    def select(self, sql):
+        logger.info('查询: %s', sql)
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute(sql)
+            data = cursor.fetchall()
+        except pymysql.Error as e:
+            logger.error('数据库select出现错误: %s', e)
+        finally:
+            cursor.close()
+        return data
+
+    def query(self, sql):
+        """
+        插入，删除，更新操作
+        :param sql:
+        :return:
+        """
+        logger.info('mysql语句: %s', sql)
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute(sql)
+            self.conn.commit()
+        except pymysql.Error as e:
+            logger.error('数据库操作出现错误: %s', e)
+            self.conn.rollback()
+        finally:
+            cursor.close()
+
+    def close(self):
+        self.conn.close()
+
+
+
