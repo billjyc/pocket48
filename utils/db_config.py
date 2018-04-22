c6d84170910c7a9b901c86265d5b8ae5b14c99e3
billjyc <billjyc@gmail.com>
2018-04-22 14:55:03 +0800

    mysql使用连接池

---
+++ utils/db_config.py
@@ -0,0 +1,32 @@
+# -*- coding: UTF-8 -*-
+
+# TEST数据库信息
+DB_HOST = "localhost"
+DB_PORT = 3306
+DB_DBNAME = "card_draw"
+DB_USER = "root"
+DB_PASSWORD = "Jyc@1993"
+
+# 数据库连接编码
+DB_CHARSET = "utf8"
+
+# mincached : 启动时开启的闲置连接数量(缺省值 0 以为着开始时不创建连接)
+DB_MIN_CACHED = 10
+
+# maxcached : 连接池中允许的闲置的最多连接数量(缺省值 0 代表不闲置连接池大小)
+DB_MAX_CACHED = 10
+
+# maxshared : 共享连接数允许的最大数量(缺省值 0 代表所有连接都是专用的)如果达到了最大数量,被请求为共享的连接将会被共享使用
+DB_MAX_SHARED = 20
+
+# maxconnecyions : 创建连接池的最大数量(缺省值 0 代表不限制)
+DB_MAX_CONNECTIONS = 100
+
+# blocking : 设置在连接池达到最大数量时的行为(缺省值 0 或 False 代表返回一个错误<toMany......>; 其他代表阻塞直到连接数减少,连接被分配)
+DB_BLOCKING = True
+
+# maxusage : 单个连接的最大允许复用次数(缺省值 0 或 False 代表不限制的复用).当达到最大数时,连接会自动重新连接(关闭和重新打开)
+DB_MAX_USAGE = 0
+
+# setsession : 一个可选的SQL命令列表用于准备每个会话，如["set datestyle to german", ...]
+DB_SET_SESSION = None
--- utils/mysql_util.py
+++ utils/mysql_util.py
@@ -3,56 +3,106 @@
 import pymysql
 from log.my_logger import logger
 
+from DBUtils.PooledDB import PooledDB
+from utils import db_config as Config
+
 
 class MySQLUtil:
-    def __init__(self, host, port, user, passwd, db):
-        self.conn = self.getConn(host, port, user, passwd, db)
+    """
+    MYSQL数据库对象，负责产生数据库连接 , 此类中的连接采用连接池实现
+    获取连接对象：conn = self.get_conn()
+    释放连接对象;conn.close()或del conn
+    """
 
-    def getConn(self, host, port, user, passwd, db, charset="utf8"):
-        logger.debug('获取数据库连接')
-        try:
-            conn = pymysql.connect(host=host, port=port, passwd=passwd, db=db, user=user, charset=charset)
-            return conn
-        except pymysql.Error as e:
-            logger.error('连接mysql出现错误: %s', e)
+    def __init__(self):
+        self._pool = PooledDB(creator=pymysql, mincached=Config.DB_MIN_CACHED, maxcached=Config.DB_MAX_CACHED,
+                              host=Config.DB_HOST, port=Config.DB_PORT, user=Config.DB_USER, passwd=Config.DB_PASSWORD,
+                              db=Config.DB_DBNAME, use_unicode=False, charset=Config.DB_CHARSET,
+                              maxshared=Config.DB_MAX_SHARED, maxconnections=Config.DB_MAX_CONNECTIONS,
+                              setsession=Config.DB_SET_SESSION)
 
-    def select(self, sql):
-        logger.info('查询: %s', sql)
-        try:
-            cursor = self.conn.cursor()
+    def get_conn(self):
+        return self._pool.connection()
+
+    # def getConn(self, host, port, user, passwd, db, charset="utf8"):
+    #     logger.debug('获取数据库连接')
+    #     try:
+    #         conn = pymysql.connect(host=host, port=port, passwd=passwd, db=db, user=user, charset=charset)
+    #         return conn
+    #     except pymysql.Error as e:
+    #         logger.error('连接mysql出现错误: %s', e)
+
+    def execute_and_get_id(self, sql, param=None):
+        """ 执行插入语句并获取自增id """
+        conn = self.get_conn()
+        cursor = conn.cursor()
+        if param is None:
             cursor.execute(sql)
-            data = cursor.fetchall()
-        except pymysql.Error as e:
-            logger.error('数据库select出现错误: %s', e)
-        finally:
-            cursor.close()
-        return data
+        else:
+            cursor.execute(sql, param)
+        row_id = cursor.lastrowid
+        cursor.close()
+        conn.close()
+        return row_id
 
-    def query(self, sql):
-        """
-        插入，删除，更新操作
-        :param sql:
-        :return:
-        """
-        logger.info('mysql语句: %s', sql)
-        try:
-            cursor = self.conn.cursor()
-            cursor.execute(sql)
-            self.conn.commit()
-        except pymysql.Error as e:
-            logger.error('数据库操作出现错误: %s', e)
-            self.conn.rollback()
-        finally:
-            cursor.close()
+    def query(self, sql, param=None):
+        """ 删除，更新，插入， 执行sql语句 """
+        conn = self.get_conn()
+        cursor = conn.cursor()
+        if param is None:
+            rowcount = cursor.execute(sql)
+        else:
+            rowcount = cursor.execute(sql, param)
+        conn.commit()
+        cursor.close()
+        conn.close()
 
-    def close(self):
-        self.conn.close()
+        return rowcount
+
+    def select_one(self, sql, param=None):
+        """ 获取一条信息 """
+        conn = self.get_conn()
+        cursor = conn.cursor()
+        rowcount = cursor.execute(sql, param)
+        if rowcount > 0:
+            res = cursor.fetchone()
+        else:
+            res = None
+        cursor.close()
+        conn.close()
+
+        return res
+
+    def select_all(self, sql, param=None):
+        """ 获取所有信息 """
+        conn = self.get_conn()
+        cursor = conn.cursor()
+        rowcount = cursor.execute(sql, param)
+        if rowcount > 0:
+            res = cursor.fetchall()
+        else:
+            res = None
+        cursor.close()
+        conn.close()
+
+        return res
 
 
 if __name__ == '__main__':
-    mysql_util = MySQLUtil('localhost', 3306, 'root', 'root', 'card_draw')
-    rst = mysql_util.select('select count(distinct(supporter_id)) from `order`')
-    print(rst[0][0])
+    mysql_util = MySQLUtil()
+    sql = """
+    select supporter_id, sum(backer_money) as total 
+    from `order` where pro_id=%s group by supporter_id order by total desc;
+    """
+    sql2 = """
+        INSERT INTO `supporter` (`id`, `name`) VALUES (%s, %s) 
+    """
+    params = (13566, )
+    params2 = (123, '熟练地')
+    rst = mysql_util.query(sql2, params2)
+    # print(rst)
+    # print(type(rst))
+    # print(rst[0][0])
 
 
 
