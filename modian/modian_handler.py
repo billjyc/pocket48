796c6226011aa54fe4c67a62578359964eea8466
billjyc <793746995@qq.com>
2018-05-30 16:44:54 +0800

    Feature 61特别活动限定PK

--- modian/modian_handler.py
+++ modian/modian_handler.py
@@ -191,7 +191,10 @@ class ModianHandler:
                         UPDATE `id`=%s
             """, (str(oid), user_id, backer_money, pay_time, modian_entity.pro_id, str(oid)))
 
-            msg = '感谢 %s 支持了%s元, %s\n' % (nickname, backer_money, util.random_str(global_config.MODIAN_POSTSCRIPTS))
+            if modian_entity.pro_id == modian_pk_handler.WJL_PRO_ID:
+                msg = '感谢 %s 支持了%s元\n' % (nickname, backer_money)
+            else:
+                msg = '感谢 %s 支持了%s元, %s\n' % (nickname, backer_money, util.random_str(global_config.MODIAN_POSTSCRIPTS))
             daka_rank, support_days = self.find_user_daka_rank(user_id, modian_entity.pro_id)
 
             if daka_rank != -1 and support_days:
