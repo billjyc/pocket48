54c80fe938b790f1233f7fd035bc402d07a7e34a
billjyc <huanying5555556666@163.com>
2018-10-16 09:16:07 +0800

    抽字活动

--- modian/modian_handler.py
+++ modian/modian_handler.py
@@ -313,10 +313,10 @@ class ModianHandler:
                     #         cards_msg += '[CQ:image,file=%s]' % k.url
                 else:
                     cards_msg += '没有抽中任何字，就送您一个“谢谢惠顾”吧'
-                # if cards_msg:
-                #     msg += '\n%s' % cards_msg
                 if cards_msg:
-                    QQHandler.send_to_groups(['483548995'], cards_msg)
+                    msg += '\n%s' % cards_msg
+                # if cards_msg:
+                #    QQHandler.send_to_groups(['483548995'], cards_msg)
 
             msg += '%s\n集资项目: %s\n链接: %s\n' % (project_info, pro_name, modian_entity.link)
             # msg += jizi_pk_report
