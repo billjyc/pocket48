474cdb0f471a6048d88a8f082dc158e29ece37b2
billjyc <793746995@qq.com>
2018-10-18 15:28:09 +0800

    概率调整

--- modian/modian_card_draw.py
+++ modian/modian_card_draw.py
@@ -58,10 +58,10 @@ class CardDrawHandler:
 
     def can_draw(self):
         """
-        是否可以抽中卡, 概率1/5
+        是否可以抽中卡, 概率1/3
         :return:
         """
-        candidates = [i for i in range(4)]
+        candidates = [i for i in range(3)]
         rst = util.choice(candidates)
         logger.debug('是否抽中卡: %s' % (rst[0] < 1))
         return rst[0] < 1
