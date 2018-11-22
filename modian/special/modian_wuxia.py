f1f767e5900696dc19364798fe94e8d5e04a1d34
billjyc <793746995@qq.com>
2018-11-22 14:50:53 +0800

    创建人物

--- modian/special/modian_wuxia.py
+++ modian/special/modian_wuxia.py
@@ -15,22 +15,22 @@ import random
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # 本地读取txt文件-list1
-last_names = util.read_txt(os.path.join(BASE_DIR, 'data', 'last_name.txt'))
-first_names = util.read_txt(os.path.join(BASE_DIR, 'data', 'first_name.txt'))
-TOTAL_NAMES = set()
-for x in itertools.product(last_names, first_names):
-    TOTAL_NAMES.add(x[0] + x[1])
+# last_names = util.read_txt(os.path.join(BASE_DIR, 'data', 'last_name.txt'))
+# first_names = util.read_txt(os.path.join(BASE_DIR, 'data', 'first_name.txt'))
+TOTAL_NAMES = util.read_txt(os.path.join(BASE_DIR, 'data', 'names.txt'))
+# for x in itertools.product(last_names, first_names):
+#     TOTAL_NAMES.add(x[0] + x[1])
 
 
 class Character:
     def __init__(self, modian_id, name, prop1=0, prop2=0, prop3=0, prop4=0, prop5=0):
         self.id = modian_id
         self.name = name
-        self.prop1 = prop1
-        self.prop2 = prop2
-        self.prop3 = prop3
-        self.prop4 = prop4
-        self.prop5 = prop5
+        self.prop1 = prop1  # 攻
+        self.prop2 = prop2  # 防
+        self.prop3 = prop3  # 气
+        self.prop4 = prop4  # 运
+        self.prop5 = prop5  # 魅力
 
 
 def created(modian_id):
@@ -62,17 +62,27 @@ def create_character(modian_id):
     random_name = TOTAL_NAMES.pop()
     my_logger.debug('随机姓名: %s' % random_name)
     # 随机生成属性
-    prop1 = random.randint(1, 10)
-    prop2 = random.randint(1, 10)
-    prop3 = random.randint(1, 10)
-    prop4 = random.randint(1, 10)
-    prop5 = random.randint(1, 10)
+    prop1 = random.randint(40, 70)
+    prop2 = random.randint(30, 60)
+    prop3 = random.randint(5, 20)
+    prop4 = random.randint(0, 50)
+    prop5 = random.randint(30, 50)
     mysql_util.query("""
         INSERT INTO `t_character` (`modian_id`, `name`, `prop1`, `prop2`, `prop3`, `prop4`, `prop5`)
             VALUES (%s, %s, %s, %s, %s, %s, %s)
     """, (modian_id, random_name, prop1, prop2, prop3, prop4, prop5))
-    report_str = '恭喜，人物创建成功！\n'
-    report_str += '姓名: %s\n属性1: %s, 属性2: %s, 属性3: %s, 属性4: %s, 属性5: %s\n' % (random_name, prop1, prop2, prop3, prop4, prop5)
+
+    intro_words = [
+        "郁雷之声渐响，轰轰不绝。河海尚远，而耳中尽是浪涛之声。 似有一名字伴雷声入耳，仔细辨来，名为：【{}】。".format(random_name),
+        "湖山深处,但见竹木阴森,苍翠重叠,不雨而润,不烟而晕, 晨雾朦胧处现一少年，“在下【{}】，请问此处可是星梦谷？”".format(random_name),
+        "月照竹林风飞叶，竹影之下见刀光。小侠籍籍无名，仅有此片竹林识得其名为【{}】".format(random_name),
+        "嗖的一声，一支羽箭从山坳后射了出来，呜呜声响，划过长空，【{}】松开弓弦，雁落平沙。".format(random_name),
+        "灯宵月夕，雪际花时，市集喧闹却也听得句柔声细语：“这文书写有{}之名，可是你掉的？”".format(random_name)
+    ]
+    # 随机挑选一个出场方式
+    intro = util.choice(intro_words)
+    report_str = '%s\n' % intro
+    report_str += '%s的武侠世界开启, 属性：\n攻: %s, 防: %s, 气: %s, 运: %s, 魅力: %s\n' % (random_name, prop1, prop2, prop3, prop4, prop5)
     return report_str
 
 
