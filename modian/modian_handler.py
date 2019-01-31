378d35c8c2f0bfe91fad6eece99bc82172912f97
billjyc <793746995@qq.com>
2019-01-31 14:22:01 +0800

    大群播报加上接棒数

--- modian/modian_handler.py
+++ modian/modian_handler.py
@@ -243,7 +243,7 @@ class ModianHandler:
                                    % (jiebang.name, jiebang.current_stick_num)
                     my_logger.debug(test_msg)
                     if len(test_msg) > 0:
-                        # msg += test_msg
+                        msg += test_msg
                         QQHandler.send_to_groups(['483548995'], test_msg)
 
             '''金额类flag相关'''
