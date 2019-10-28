98b760886eabb7b2da30f03ee33146efe7cb8954
billjyc <billjyc@gmail.com>
2019-10-28 23:47:56 +0800

    图片查询

--- cool_http_server.py
+++ cool_http_server.py
@@ -342,7 +342,7 @@ def search_card(context, modian_id):
             return
         report = card_draw_handler.get_cards(int(modian_id))
         bot.send(context, report)
-        bot.send(context, '[CQ:image,file={}]'.format('result.png'))
+        bot.send(context, '[CQ:image,file=%s]' % 'result.png')
     except Error as e:
         logger.error(e)
         # bot.send(context, '查询出现错误！\n{}'.format(traceback.print_exc()))
