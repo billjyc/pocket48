cdb6bc0e8035c491d4ff1c4fac236a48f2130142
billjyc <billjyc@gmail.com>
2020-04-02 07:48:25 +0800

    应援周榜统计

---
+++ pocket48/melee_week_rank.py
@@ -0,0 +1,98 @@
+# -*- coding:utf-8 -*-
+import json
+import os
+import time
+
+import requests
+import xlwt
+from utils import util
+import traceback
+from utils import global_config
+import urllib.request
+
+
+rst = {}
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+workbook = xlwt.Workbook(encoding='utf-8')
+worksheet = workbook.add_sheet('My Worksheet')
+
+def get_header():
+    header = {
+        'Content-Type': 'application/json;charset=utf-8',
+        'User-Agent': 'PocketFans201807/6.0.11 (iPhone; iOS 13.3; Scale/2.00)',
+        'appInfo': json.dumps({
+            'vendor': 'apple',
+            'deviceId': 0,
+            "appVersion": "6.0.10",
+            "appBuild": "200120",
+            "osVersion": "13.3.0",
+            "osType": "ios",
+            "deviceName": "iphone",
+            "os": "ios"
+        }),
+        # 'token': global_config.POCKET48_TOKEN
+        'token': 'k8EEcQjKCZGx4XO8v9O5PH8VlHZBuR8fB5be4WgQSredPbUZ1ryPDeFhRrrGv/Sv1SCg/JdDPrk='
+    }
+    return header
+
+
+def get_single_week_rank(week_id):
+    """
+    获取单周的应援榜
+    :param week_id:
+    :return:
+    """
+    url = 'https://pocketapi.48.cn/gift/api/v1/melee/rank/getMeleeWeekRank'
+    next_id = 0
+    pre_next_id = -1
+
+    global rst
+    print(week_id)
+    time.sleep(5)
+
+    while next_id != pre_next_id:
+        print(next_id)
+        time.sleep(1)
+        try:
+            params = {
+                "rankId": week_id,
+                "nextId": next_id,
+            }
+            r = requests.post(url, data=json.dumps(params), headers=get_header(), verify=False).json()
+            print(r)
+
+            if r["status"] == 200:
+                rank_user_list = r["content"]["rankUserList"]
+                for user in rank_user_list:
+                    user_id = str(user['baseUserInfo']['userId'])
+                    user_name = user['baseUserInfo']['nickname']
+                    melee = user['melee']
+                    if user_id not in rst:
+                        rst[user_id] = {
+                            'name': user_name,
+                            'melee': melee
+                        }
+                    else:
+                        rst[user_id]['melee'] = rst[user_id]['melee'] + melee
+                pre_next_id = next_id
+                next_id = r["content"]["nextId"]
+        except Exception as e:
+            print(e)
+    print(rst)
+
+
+if __name__ == "__main__":
+    line = 1
+    worksheet.write(0, 0, 'ID')
+    worksheet.write(0, 1, '姓名')
+    worksheet.write(0, 2, '鸡腿数')
+    for i in range(71, 84):
+        get_single_week_rank(i)
+
+    for k, v in rst.items():
+        worksheet.write(line, 0, k)
+        worksheet.write(line, 1, v['name'])
+        worksheet.write(line, 2, v['melee'])
+        line += 1
+
+    workbook.save('melee.xls')
