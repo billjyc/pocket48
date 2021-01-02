0437795860336c675ed155cd10354d472fa118c0
billjia <billjia@tencent.com>
2021-01-02 21:34:40 +0800

    update

---
+++ mirai_bot.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+
+from graia.broadcast import Broadcast
+from graia.application import GraiaMiraiApplication, Session
+from graia.application.message.chain import MessageChain
+import asyncio
+
+from graia.application.message.elements.internal import Plain
+from graia.application.friend import Friend
+
+loop = asyncio.get_event_loop()
+
+bcc = Broadcast(loop=loop)
+app = GraiaMiraiApplication(
+    broadcast=bcc,
+    connect_info=Session(
+        host="http://localhost:8080",  # 填入 httpapi 服务运行的地址
+        authKey="graia-mirai-api-http-authkey",  # 填入 authKey
+        account=421497163,  # 你的机器人的 qq 号
+        websocket=True  # Graia 已经可以根据所配置的消息接收的方式来保证消息接收部分的正常运作.
+    )
+)
+
+
+@bcc.receiver("FriendMessage")
+async def friend_message_listener(app: GraiaMiraiApplication, friend: Friend):
+    await app.sendFriendMessage(friend, MessageChain.create([
+        Plain("Hello, World!")
+    ]))
+
+
+app.launch_blocking()
