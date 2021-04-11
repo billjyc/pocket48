ed3e8a23d7e9f5bf8c750329a1d6b86257a2b729
billjia <billjia@tencent.com>
2021-04-11 23:38:02 +0800

    统计

--- pocket48/get_room_history_msgs.py
+++ pocket48/get_room_history_msgs.py
@@ -6,11 +6,15 @@ import os
 import time
 from utils import util
 from log.my_logger import pocket48_logger as logger
+import m3u8
 
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 db_path = os.path.join(base_dir, 'statistic', 'statistics.db')
 conn = sqlite3.connect(db_path, check_same_thread=False)
 cursor = conn.cursor()
+
+total_time = 0
+total_online_num = 0
 
 cursor.execute("""
                 CREATE TABLE IF NOT EXISTS room_message (
@@ -47,20 +51,88 @@ class TextMessageType:
 def get_header():
     header = {
         'Content-Type': 'application/json;charset=utf-8',
-        'User-Agent': 'PocketFans201807/6.0.15 (iPhone; iOS 13.3; Scale/2.00)',
+        'User-Agent': 'PocketFans201807/6.1.1 (iPad; iOS 14.4; Scale/2.00)',
         'appInfo': json.dumps({
             'vendor': 'apple',
             'deviceId': 0,
-            "appVersion": "6.0.15",
-            "appBuild": "200120",
-            "osVersion": "13.3.0",
+            "appVersion": "6.1.1",
+            "appBuild": "210202",
+            "osVersion": "14.4.0",
             "osType": "ios",
             "deviceName": "iphone",
             "os": "ios"
         }),
-        'token': "3yMRrRZMgdFvcsWZpV2Uxe1SaN4DsbW4Fbezlk3IPXPaOTS/Fn9YNv6CbHzFqt6HaWG/froKMW0="
+        'token': "1xc3SYA7ZIjAXsDLu7Xgthg8YCwqswa5OCTh3ypGW4my7yDLscpsnTsnQCte/rnz3IL4TJ/QG/s="
     }
     return header
+
+
+def get_room_history_live(member_id, room_id, start_time):
+    next_start_time = _get_room_live(member_id, room_id, start_time, 30)
+    while next_start_time != -1:
+        next_start_time = _get_room_live(member_id, room_id, next_start_time, 30)
+    print('done')
+
+
+def get_live_one(live_id):
+    time.sleep(1)
+    global total_time, total_online_num
+    url = 'https://pocketapi.48.cn/live/api/v1/live/getLiveOne'
+    params = {
+        "liveId": live_id,
+    }
+    try:
+        r = requests.post(url, data=json.dumps(params), headers=get_header(), verify=False).text
+    except Exception as e:
+        print(e)
+
+    rsp_json = json.loads(r)
+    infos = rsp_json['content']
+    online_num = infos['onlineNum']
+    total_online_num += online_num
+    play_stream_path = infos['playStreamPath']
+    print(live_id)
+    print(play_stream_path)
+
+    playlist = m3u8.load(play_stream_path, verify_ssl=False)
+    for segment in playlist.data['segments']:
+        total_time += segment['duration']
+
+
+def _get_room_live(member_id, room_id, last_time, limit):
+    time.sleep(3)
+    url = 'https://pocketapi.48.cn/im/api/v1/chatroom/msg/list/aim/type'
+    params = {
+        "extMsgType": "USER_LIVE",
+        "roomId": room_id,
+        "ownerId": member_id,
+        "nextTime": last_time
+    }
+    try:
+        r = requests.post(url, data=json.dumps(params), headers=get_header(), verify=False).text
+    except Exception as e:
+        print(e)
+
+    rsp_json = json.loads(r)
+    msgs = rsp_json['content']['message']
+    if len(msgs) == 0:
+        return -1
+    cursor = conn.cursor()
+    msg_time = last_time
+    try:
+        for msg in msgs:
+            extInfo = json.loads(msg['extInfo'])
+            msg_id = msg['msgidClient']  # 消息id
+            live_id = extInfo['id']
+
+            get_live_one(live_id)
+            msg_time = msg['msgTime']
+    except Exception as e:
+        print(e)
+    finally:
+        conn.commit()
+        cursor.close()
+        return rsp_json['content']['nextTime']
 
 
 def get_room_history_msg(member_id, room_id, start_time):
@@ -249,5 +321,12 @@ def get_idol_flip_name(answer_id, question_id):
 
 if __name__ == '__main__':
     import time
-
-    get_room_history_msg(6432, 67246079, 0)
+    # path = 'https://cychengyuan-vod.48.cn/42402830/20210411/cy/588101487630815232-g2l4jmcrls26wvcy1iv9.m3u8'
+    # playlist = m3u8.load(path, verify_ssl=False)
+    # for segment in playlist.data['segments']:
+    #     total_time += segment['duration']
+    # print(total_time)
+    # get_room_history_msg(6432, 67246079, 0)
+    get_room_history_live(42402868, 248602943, 0)
+    print('总时间: {}'.format(total_time))
+    print('总人数: {}'.format(total_online_num))
